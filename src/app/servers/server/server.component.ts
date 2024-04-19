51db77782798b6e9dd7981e839fe3befbc9cdc99
Nicky Wang <shuowang823@gmail.com>
2024-04-19 17:09:20 +0800

    feat: add console.log to know lifecycle

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,23 +1,28 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.scss'],
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
   @Output() removeServer = new EventEmitter<string>();
   @Input() serverName: string;
   @Input() serverId: string;
   serverStatus: boolean;
 
-  onClickRemoveBtn() {
-    return this.removeServer.emit(this.serverId);
-  }
-
   constructor() {
     this.serverName = '';
     this.serverId = Math.floor(Math.random() * 100).toString();
     this.serverStatus = Math.random() > 0.5;
+    console.log('constrctor:', this.serverId);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit:', this.serverId);
+  }
+
+  onClickRemoveBtn() {
+    return this.removeServer.emit(this.serverId);
   }
 }
